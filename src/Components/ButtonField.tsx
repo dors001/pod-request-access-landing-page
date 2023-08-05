d463143493b116b1b2800e3653a2085676599e90
dors001 <dorarishani@gmail.com>
2023-08-05 03:41:25 +0300

    Define ButtonField component

---
+++ src/Components/ButtonField.tsx
@@ -0,0 +1,21 @@
+import { Box } from "@chakra-ui/react";
+
+const ButtonField = () => {
+  return (
+    <div>
+      <Box
+        as="button"
+        width="326px"
+        height="46px"
+        color="#121725"
+        fontWeight="bold"
+        backgroundColor="#54E6AF"
+        borderRadius="25px"
+      >
+        Request Access
+      </Box>
+    </div>
+  );
+};
+
+export default ButtonField;
